c4b226866b67586aef6a8074742430a4d9c13708
beep-boi <engineerabsaar@gmail.com>
2026-08-23 19:59:02 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'hisaab-v2';
+var CACHE = 'hisaab-v3';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
